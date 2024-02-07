f7062cf75ea40790050785e82e7e4c54553d8538
szymi-zap <110560339+szymi-zap@users.noreply.github.com>
2024-02-07 18:41:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,16 +26,18 @@ client.on('ready', async () => {
     .setApplicationId('1087940913348743189')
     .setType('STREAMING')
     .setURL('https://www.twitch.tv/losfutbolitos') //Must be a youtube video link 
-    .setState('😞😞')
-    .setName('😞😞')
-    .setDetails(`😞😞 [${formatTime()}]`)
+    
+    .setName('/yxn')
+    .setDetails(`/yxn [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://media.discordapp.net/attachments/1127012472025522287/1200926451000692746/d3e797ca094054a1ba0c1ff20ab00b52.jpg?ex=65c7f4ea&is=65b57fea&hm=ef7e046f0eae84d3a5d661d7f36f3a2f18b83e08a571f5ca438a91f3874e3abf&=&format=webp&width=261&height=409') //You can put links in tenor or discord and etc.
-    .setAssetsLargeText('Crying') //Text when you hover the Large image
-    .setAssetsSmallImage('https://media.discordapp.net/attachments/1127012472025522287/1200929423722037300/448101099b44089b0169c5cd30fe0106.jpg?ex=65c7f7ae&is=65b582ae&hm=2f92f8b539983d15cee13abd89b69053e0409eb97e2e664fe1a6ba1da22b8d08&=&format=webp&width=184&height=409') //You can put links in tenor or discord and etc.
-    .setAssetsSmallText('y ahora¿?') //Text when you hover the Small image
-    .addButton('BlackParty♱', 'https://discord.gg/UKpgfW5J')
-    .addButton('Legacys♱', 'https://discord.gg/HvsYb4kK');
+
+    
+
+    .setAssetsLargeImage('https://cdn.discordapp.com/attachments/925574783901528094/1185965702297960538/healbanner_AxWFEfGg.gif?ex=65d22121&is=65bfac21&hm=ba1c84175dbf4ce188ca9391de4b99115a758362a80994b5c5399c16ef91c6db&')
+    .addButton('/yxn','https://discord.gg/yxn')
+    .addButton('/blackparty','https://discordd.gg/blackparty')
+    
+
 
   client.user.setActivity(r);
   client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
@@ -44,7 +46,7 @@ client.on('ready', async () => {
   setInterval(() => {
     const newTime = formatTime();
     if (newTime !== prevTime) {
-      const newDetails = `Llorando por Xavi`;
+      const newDetails = `/yxn`;
       r.setDetails(newDetails);
       client.user.setActivity(r);
       prevTime = newTime;
