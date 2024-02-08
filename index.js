44e3cd2d1a92d16a8cb565c8edb66b9cde8bc86b
szymi-zap <110560339+szymi-zap@users.noreply.github.com>
2024-02-08 13:07:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js-selfbot-v13');
+ const Discord = require('discord.js-selfbot-v13');
 const client = new Discord.Client({
   readyStatus: false,
   checkUpdate: false
@@ -25,7 +25,7 @@ client.on('ready', async () => {
   const r = new Discord.RichPresence()
     .setApplicationId('1087940913348743189')
     .setType('STREAMING')
-    .setURL('https://www.twitch.tv/mongraal') //Must be a youtube video link 
+    .setURL('https://www.twitch.tv/losfutbolitos') //Must be a youtube video link 
     
     .setName('/yxn')
     .setDetails(`/yxn [${formatTime()}]`)
@@ -36,7 +36,7 @@ client.on('ready', async () => {
     .setAssetsLargeImage('https://media.discordapp.net/attachments/1145698570159271999/1192890000510029824/ANFBKqz.gif?ex=65cfa262&is=65bd2d62&hm=1341d47fda453f33be6c1f22d76dabb95d2457bc0efa4047d145eb553e95361f&')
     .addButton('/yxn','https://discord.gg/yxn')
     .addButton('/27x','https://discord.gg/27x')
-    .addButton('/priv de priv','https://discord.gg/privdepriv')
+    
 
 
   client.user.setActivity(r);
